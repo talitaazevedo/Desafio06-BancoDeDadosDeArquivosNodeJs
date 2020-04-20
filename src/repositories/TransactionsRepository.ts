080cd2aeabc8f198d40f78f3609f55b9a78f25c5
Talita Azevedo <talita.azevedo360@gmail.com>
2020-04-19 21:47:04 -0300

    Transaction and Category Repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const incomeData = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc, t) => acc + t.value, 0);
+
+    const outcomeData = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc, t) => acc + t.value, 0);
+
+    const result = incomeData - outcomeData;
+
+    return {
+      income: incomeData,
+      outcome: outcomeData,
+      total: result,
+    };
   }
 }
 
